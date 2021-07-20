64279202ecf1cf4c57f90bd13be37833ff49adfb
Raúl Emmanuel Juárez Parra <jpre11@hotmail.com>
2021-07-20 11:24:10 -0500

    Se agrego el encabeza para aceptar el access-token en el header

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3001;
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
