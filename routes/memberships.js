538e1f2fc7cbeffad1a5c91c13af78e52b128183
Raúl Emmanuel Juárez Parra <jpre11@hotmail.com>
2021-08-28 15:23:09 -0500

    Corrección cierre de base de datos conexión

--- routes/memberships.js
+++ routes/memberships.js
@@ -14,14 +14,14 @@ router.get('/api/memberships/getmembershiplist', function(request, response){
     mssql.connect(sqlConnect.dbconnection()).then(() => {
         return new mssql.Request().execute("Usp_API_CatalogoMembresiasRecuperar")
     }).then(result => {
-        mssql.close()
         
         console.log(result.recordset)
         response.status(200).send(result.recordset)
     }).catch(error => {
         response.status(500).send('Ocurrio un error al intentar conectarse con el servicio. Intente mas tarde.')
-        mssql.close()
     })
+
+    mssql.close()
 })
 
 router.post('/api/createCustomer', function(req, res){
