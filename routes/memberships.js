d7edf242ebec18cebfcfb341bbe6d5f534b80560
Raúl Emmanuel Juárez Parra <jpre11@hotmail.com>
2021-09-03 17:21:31 -0500

    Correccion

--- routes/memberships.js
+++ routes/memberships.js
@@ -11,14 +11,11 @@ conekta.api_key = 'key_THEXVyarQFRAkMmoLJbpGw'; //  <-- Mock private key, please
 conekta.api_version = '2.0.0';
 
 router.get('/api/memberships/getmembershiplist', function(request, response){
-    mssql.connect(sqlConnect.dbconnection()).then(() => {
+    new mssql.connect(sqlConnect.dbconnection()).then(() => {
         return new mssql.Request().execute("Usp_API_CatalogoMembresiasRecuperar")
     }).then(result => {
-        
-        console.log(result.recordset)
         response.status(200).send(result.recordset)
-
-         
+        mssql.close()
     }).catch(error => {
         response.status(500).send('Ocurrio un error al intentar conectarse con el servicio. Intente mas tarde.')
          
