07a6bddffccf300d2ee0f36ad0f7c1eee2d567ec
Raúl Emmanuel Juárez Parra <jpre11@hotmail.com>
2021-09-07 17:44:53 -0500

    correcion

--- routes/account.js
+++ routes/account.js
@@ -87,7 +87,7 @@ router.post('/api/user/login', async (request, response) => {
     let username = request.body.CorreoUsuario;
 
     //se valida el inicio de sesión  del usuario
-    new mssql.connect(sqlConnect.dbconnection()).then(() => {
+    mssql.connect(sqlConnect.dbconnection()).then(() => {
         return new mssql.Request()
             .input('CorreoUsuario', username)
             .execute("Usp_API_IniciarSesionRecuperar")
