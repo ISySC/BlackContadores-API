b459064ff13f3961125c78bf6706300ff4788e08
softm-rauljuarez <raul.juarez.ce@softm.com.mx>
2023-07-20 09:57:27 -0600

    cambio de puerto

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const fs = require('fs');
-const https = require('https');
+//const https = require('https');
+const http = require('http')
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3003;
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -29,10 +30,13 @@ app.use(user)
 app.use(payment)
 
 //configuración para SSL 
-const privateKey = fs.readFileSync( '/etc/letsencrypt/live/www.blacksystem.mx/privkey.pem' );
-const certificate = fs.readFileSync( '/etc/letsencrypt/live/www.blacksystem.mx/fullchain.pem' );
+//const privateKey = fs.readFileSync( '/etc/letsencrypt/live/www.blacksystem.mx/privkey.pem' );
+//const certificate = fs.readFileSync( '/etc/letsencrypt/live/www.blacksystem.mx/fullchain.pem' );
 
-https.createServer({
+/*https.createServer({
             key: privateKey,
             cert: certificate
-}, app).listen(port);
+}, app).listen(port);*/
+var server = app.listen(port, function () {
+    console.log('Server running on port:', port)
+})
